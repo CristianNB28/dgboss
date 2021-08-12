b006cf9d198d9aa5db667ee5fe81cd7583f1192a
Christian Neira <cristianneira07@gmail.com>
2021-08-12 17:40:43 -0400

    Cambios de codigo para despliegue en clever cloud

--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,24 @@ const db = require('../../config/database');
 
 module.exports = {
     postLogin: (correo_usuario) => {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
-                if (error) {
-                    reject(error)
-                }
-                resolve(rows);
+        if (process.env.PRODUCTION == 'false') {
+            return new Promise((resolve, reject) => {
+                db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
+                    if (error) {
+                        reject(error)
+                    }
+                    resolve(rows);
+                });
             });
-        });
+        } else if (process.env.PRODUCTION == 'true'){
+            return new Promise((resolve, reject) => {
+                db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM bjo4oss6ie2anbdpo0b0.usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
+                    if (error) {
+                        reject(error)
+                    }
+                    resolve(rows);
+                });
+            });
+        }
     }
 }
