83752e9fc5920113247e9b354108120cebb0e463
Christian Neira <cristianneira07@gmail.com>
2021-08-12 18:20:17 -0400

    Cambio en el codigo

--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,13 @@ const db = require('../../config/database');
 
 module.exports = {
     postLogin: (correo_usuario) => {
-        if (process.env.PRODUCTION == 'false') {
-            return new Promise((resolve, reject) => {
-                db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
-                    if (error) {
-                        reject(error)
-                    }
-                    resolve(rows);
-                });
+        return new Promise((resolve, reject) => {
+            db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM Usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
+                if (error) {
+                    reject(error)
+                }
+                resolve(rows);
             });
-        } else if (process.env.PRODUCTION == 'true'){
-            return new Promise((resolve, reject) => {
-                db.query('SELECT id_usuario, correo_usuario, password_usuario, nombre_usuario FROM bjo4oss6ie2anbdpo0b0.usuario WHERE correo_usuario = ?', [correo_usuario], (error, rows) => {
-                    if (error) {
-                        reject(error)
-                    }
-                    resolve(rows);
-                });
-            });
-        }
+        });
     }
 }
